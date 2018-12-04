4e23a9c3833a10030fa62ad54a9e6c075153e799
mcerdeiro <mcerdeiro@gmail.com>
2018-12-04 14:14:53 +0100

    day 4 completed

--- day3.ts
+++ day3.ts
@@ -1,116 +1,150 @@
 import { dataTest } from "./data";
 import { data } from "./data";
 
-function getData(val: string): number[] {
-    let id: number;
-    const pos: number[] = [0, 0];
-    const size: number[] = [0, 0];
+enum Type {guard, wake, sleep };
 
-    const val_split = val.split(' '); // #1 @ 1,3: 4x4 -> '#1' '@' '1,3:' '4x4'
-    id = Number(val_split[0].split('#')[1]);
-    pos[0] = Number(val_split[2].split(',')[0]);
-    pos[1] = Number(val_split[2].split(',')[1].split(':')[0]);
-    size[0] = Number(val_split[3].split('x')[0]);
-    size[1] = Number(val_split[3].split('x')[1]);
+function getType(line: string): Type {
+    if (line.indexOf('Guard #') > 0) {
+        return Type.guard;
+    }
+    if (line.indexOf('wakes up') > 0) {
+        return Type.wake;
+    }
+    if (line.indexOf('falls asleep') > 0) {
+        return Type.sleep
+    }
+    console.log('this shall not happens');
+    while(1);
+}
 
-    console.log('Id: ' + id + ' x: ' + pos[0] + ' y:' + pos[1] + ' wide: ' + size[0] + ' high: ' + size[1]);
+function getGuard(line: string): number {
+    // '[1518-02-05 00:00] Guard #3109 begins shift',
+    const id = line.split('#')[1].split(' ')[0];
+    return Number(id);
+}
 
-    return [id, pos[0], pos[1], size[1], size[0]];
+function getMinutes(line: string): number {
+    // '[1518-02-05 00:50] falls asleep',
+    const minutes = line.split(']')[0].split(':')[1];
+    return Number(minutes);
 
+}
+
+function processData(data: number[][], guard: number, sleep: number, wake: number): void {
+    for (let i = sleep; i < wake; i++) {
+        if (data[guard] === undefined) {
+            data[guard] = Array<number>();
+            data[guard][i] = 1;
+        } else if (data[guard][i] === undefined) {
+            data[guard][i] = 1;
+        } else {
+            data[guard][i]++;
+        }
+    }
+
+    console.log('Guard: ' + guard);
+    console.log('000000000011111111112222222222333333333344444444445555555555');
+    console.log('012345678901234567890123456789012345678901234567890123456789');
+    let toprint = '';
+    for (let i = 0; i < 61; i++) {
+        if (data[guard][i] > 0) {
+            toprint += '#'
+        } else {
+            toprint += '.'
+        }
+    }
+    console.log(toprint);
 }
 
 function execute() {
-    const area = new Area();
-    for (const val1 of data) {
-        console.log('Processing: ' + val1);
-        const tmp = getData(val1);
-        area.cuadrado(tmp[1], tmp[2], tmp[3], tmp[4]);
+    let storage: number[][];
+    let guard: number;
+    let sleep: number;
+    let wake: number;
+    let maxId = 0;
+
+    storage = Array();
+    for(const line of data) {
+        // console.log('processing: ' + line);
+        switch (getType(line)) {
+            case Type.guard:
+                guard = getGuard(line);
+                // console.log('Guard ' + guard + ' started shift...');
+                break;
+            case Type.sleep:
+                sleep = getMinutes(line);
+                // console.log('Guard ' + guard + ' sleept on ' + sleep);
+                break;
+            case Type.wake:
+                if (sleep < 0) {
+                    console.log('error this shall not occur');
+                    while(1);
+                }
+                wake = getMinutes(line);
+                console.log('Guard ' + guard + ' sleept on ' + sleep + ' and weeks up on ' + wake);
+                processData(storage, guard, sleep, wake);
+                if (maxId < guard) {
+                    maxId = guard;
+                }
+                sleep = -1;
+                break;
+
+        }
+
     }
 
-    console.log('ovelap: ' + area.countOverlap());
-
-    for (const val1 of data) {
-        console.log('Processing: ' + val1);
-        const tmp = getData(val1);
-        if (area.checkOverlap(tmp[1], tmp[2], tmp[3], tmp[4])) {
-            console.log('ID: ' + tmp[0] + ' does not overlap...');
-            return;
+    let MaxTime = 0;
+    let Id: number;
+    for(let i = 0; i <= maxId; i++) {
+        let tmp = 0;
+        for(let j = 0; j < 60; j++) {
+            if ((storage[i] !== undefined) && (storage[i][j] !== undefined)) {
+                tmp += storage[i][j];
+            }
+        }
+        console.log('Guard #' + i + ' sleeped for ' + tmp + 'minutes...');
+        if (tmp > MaxTime) {
+            MaxTime = tmp;
+            Id = i;
         }
     }
+    console.log('Max Guard #' + Id + ' sleeped for ' + MaxTime + 'minutes...');
+
+    let whoof = 0;
+    let min = 0;
+
+    for(let j = 0; j < 60; j++) {
+        if ((storage[Id] !== undefined) && (storage[Id][j] !== undefined)) {
+            if (whoof < storage[Id][j]) {
+                whoof = storage[Id][j];
+                min = j;               
+            }
+        }
+    }
+    console.log('Guard #' + Id + ' was sleeped ' + whoof + ' at minute ' + min);
+
+    console.log('Result: ' + Id*min);
+
+
+    let p2_Id = 0;
+    let p2_Min = 0;
+    let p2_WO = 0;
+    for(let i = 0; i <= maxId; i++) {
+        let tmp = 0;
+        for(let j = 0; j < 60; j++) {
+            if ((storage[i] !== undefined) && (storage[i][j] !== undefined)) {
+                if (storage[i][j] > p2_WO) {
+                    p2_WO = storage[i][j];
+                    p2_Min = j;
+                    p2_Id = i;
+                }
+            }
+        }
+    }
+    console.log('The ID#' + p2_Id + ' sleeped ' + p2_WO + ' at minute ' + p2_Min);
+    console.log('Result: ' + p2_Id* p2_Min);
+
 }
 
-class Area {
-    area_colector: number[][];
-    maxx = 0;
-    maxy = 0;
-
-    constructor() {
-        this.area_colector = new Array();
-    }
-  
-    setVal(x: number, y: number) {
-      if (this.area_colector[x] === undefined) {
-        this.area_colector[x] = new Array<number>();
-        this.area_colector[x][y] = 1;
-        // console.log('Setted1: ' + x + ' ' + y + ': ' + this.area_colector[x][y]);
-      } else if (this.area_colector[x][y] === undefined) {
-        this.area_colector[x][y] = 1;
-        // console.log('Setted2: ' + x + ' ' + y + ': ' + this.area_colector[x][y]);
-      }
-      else {
-        this.area_colector[x][y] = this.area_colector[x][y] + 1;
-        // console.log('Setted3: ' + x + ' ' + y + ': ' + this.area_colector[x][y]);
-      }
-      // console.log('Setted: ' + x + ' ' + y + ': ' + this.area_colector[x][y]);
-  
-      if (x > this.maxx) {
-        this.maxx = x;
-      }
-      if (y > this.maxy) {
-        this.maxy = y;
-      }
-    }
-  
-    cuadrado(x: number, y: number, w: number, h: number) {
-      console.log('Cuadr: ' + x + ' ' + y + ' - ' + w + ' ' + h);
-      for (let i = 0; i < w; i++) {
-        for (let j = 0; j < h; j++) {
-          this.setVal(x + j, y + i);
-        }
-      }
-    }
-
-    checkOverlap(x: number, y: number, w: number, h: number): boolean {
-      console.log('Cuadr: ' + x + ' ' + y + ' - ' + w + ' ' + h);
-      for (let i = 0; i < w; i++) {
-        for (let j = 0; j < h; j++) {
-          // console.log('Overlap in: ' + i + ' ' + j + ' overlap: ' + this.area_colector[i][j]);
-          if (this.area_colector[x+j][y+i] > 1) {
-            return false;
-          }
-        }
-      }
-      return true;
-    }
-  
-    countOverlap(): number {
-      let ret = 0;
-      for (let i = 0; i <= this.maxx; i++) {
-        for (let j = 0; j <= this.maxy; j++) {
-          // console.log('Cechking: ' + i + ' ' + j);
-          if ( (this.area_colector[i] !== undefined) && (this.area_colector[i][j] !== undefined) ) {
-            if (this.area_colector[i][j] > 1) {
-                console.log('Overlap in: ' + i + ' ' + j + ' overlap: ' + this.area_colector[i][j]);
-                ret++;
-            }
-          }
-        }
-      }
-      return ret;
-    }
-  
-  }
-
 execute();
-
 
