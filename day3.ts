02c7265ddc67c279cbdfdfa3f06ed81ce1ae8350
mcerdeiro <mcerdeiro@gmail.com>
2018-12-07 19:31:47 +0100

    Day 6

--- day3.ts
+++ day3.ts
@@ -1,150 +1,140 @@
 import { dataTest } from "./data";
 import { data } from "./data";
 
-enum Type {guard, wake, sleep };
 
-function getType(line: string): Type {
-    if (line.indexOf('Guard #') > 0) {
-        return Type.guard;
-    }
-    if (line.indexOf('wakes up') > 0) {
-        return Type.wake;
-    }
-    if (line.indexOf('falls asleep') > 0) {
-        return Type.sleep
-    }
-    console.log('this shall not happens');
-    while(1);
-}
+class Storage {
+    storage: number[][];
 
-function getGuard(line: string): number {
-    // '[1518-02-05 00:00] Guard #3109 begins shift',
-    const id = line.split('#')[1].split(' ')[0];
-    return Number(id);
-}
-
-function getMinutes(line: string): number {
-    // '[1518-02-05 00:50] falls asleep',
-    const minutes = line.split(']')[0].split(':')[1];
-    return Number(minutes);
-
-}
-
-function processData(data: number[][], guard: number, sleep: number, wake: number): void {
-    for (let i = sleep; i < wake; i++) {
-        if (data[guard] === undefined) {
-            data[guard] = Array<number>();
-            data[guard][i] = 1;
-        } else if (data[guard][i] === undefined) {
-            data[guard][i] = 1;
-        } else {
-            data[guard][i]++;
-        }
-    }
-
-    console.log('Guard: ' + guard);
-    console.log('000000000011111111112222222222333333333344444444445555555555');
-    console.log('012345678901234567890123456789012345678901234567890123456789');
-    let toprint = '';
-    for (let i = 0; i < 61; i++) {
-        if (data[guard][i] > 0) {
-            toprint += '#'
-        } else {
-            toprint += '.'
-        }
-    }
-    console.log(toprint);
-}
-
-function execute() {
-    let storage: number[][];
-    let guard: number;
-    let sleep: number;
-    let wake: number;
-    let maxId = 0;
-
-    storage = Array();
-    for(const line of data) {
-        // console.log('processing: ' + line);
-        switch (getType(line)) {
-            case Type.guard:
-                guard = getGuard(line);
-                // console.log('Guard ' + guard + ' started shift...');
-                break;
-            case Type.sleep:
-                sleep = getMinutes(line);
-                // console.log('Guard ' + guard + ' sleept on ' + sleep);
-                break;
-            case Type.wake:
-                if (sleep < 0) {
-                    console.log('error this shall not occur');
-                    while(1);
-                }
-                wake = getMinutes(line);
-                console.log('Guard ' + guard + ' sleept on ' + sleep + ' and weeks up on ' + wake);
-                processData(storage, guard, sleep, wake);
-                if (maxId < guard) {
-                    maxId = guard;
-                }
-                sleep = -1;
-                break;
-
-        }
-
-    }
-
-    let MaxTime = 0;
-    let Id: number;
-    for(let i = 0; i <= maxId; i++) {
-        let tmp = 0;
-        for(let j = 0; j < 60; j++) {
-            if ((storage[i] !== undefined) && (storage[i][j] !== undefined)) {
-                tmp += storage[i][j];
-            }
-        }
-        console.log('Guard #' + i + ' sleeped for ' + tmp + 'minutes...');
-        if (tmp > MaxTime) {
-            MaxTime = tmp;
-            Id = i;
-        }
-    }
-    console.log('Max Guard #' + Id + ' sleeped for ' + MaxTime + 'minutes...');
-
-    let whoof = 0;
-    let min = 0;
-
-    for(let j = 0; j < 60; j++) {
-        if ((storage[Id] !== undefined) && (storage[Id][j] !== undefined)) {
-            if (whoof < storage[Id][j]) {
-                whoof = storage[Id][j];
-                min = j;               
+    constructor([x, y], init = 0) {
+        x++;y++;
+        console.log('new storage for x: ' + x + ' y:' + y);
+        this.storage = new Array(x);
+        for (let i = 0; i < x; i++) {
+            this.storage[i] = new Array(y);
+            for ( let j = 0; j < y; j++) {
+                this.storage[i][j] = init;
             }
         }
     }
-    console.log('Guard #' + Id + ' was sleeped ' + whoof + ' at minute ' + min);
 
-    console.log('Result: ' + Id*min);
-
-
-    let p2_Id = 0;
-    let p2_Min = 0;
-    let p2_WO = 0;
-    for(let i = 0; i <= maxId; i++) {
-        let tmp = 0;
-        for(let j = 0; j < 60; j++) {
-            if ((storage[i] !== undefined) && (storage[i][j] !== undefined)) {
-                if (storage[i][j] > p2_WO) {
-                    p2_WO = storage[i][j];
-                    p2_Min = j;
-                    p2_Id = i;
-                }
+    display() {
+        for (let i = 0; i < this.storage.length; i++) {
+            let tmp = '';
+            for (let j = 0; j < this.storage[i].length; j++) {
+                tmp += this.storage[i][j];
             }
+            console.log('    ' + tmp);
         }
     }
-    console.log('The ID#' + p2_Id + ' sleeped ' + p2_WO + ' at minute ' + p2_Min);
-    console.log('Result: ' + p2_Id* p2_Min);
+    
+}
+
+function getCoo(line: string): [number, number] {
+    line.replace(' ', '');
+    let res = line.split(',');
+
+    return [Number(res[0]), Number(res[1])];
 
 }
 
-execute();
+function getMax(data): [number, number] {
+    let ret = [0, 0];
+    for (let line of data) {
+        let c = getCoo(line);
+        if (c[0] > ret[0]) ret[0] = c[0];
+        if (c[1] > ret[1]) ret[1] = c[1];
+    }
+
+    return [ret[0], ret[1]];
+}
+
+function getDistance(c1, c2): number {
+    let dist1 = c1[0] - c2[0];
+    let dist2 = c1[1] - c2[1];
+
+    if (dist1 < 0) dist1 *= -1;
+    if (dist2 < 0) dist2 *= -1;
+
+    return dist1 + dist2;
+}
+
+function getIdMinimalDistance(data, c1): number {
+    let id = 0;
+    let minDist = 1000000000000000000000000;
+    let minDistId = 0;
+    for (let line of data) {
+        id++;
+
+        let c2 = getCoo(line);
+        let tmpdist = getDistance(c1, c2);
+        // console.log('Distance: ' + c1 + ' to ' + c2 + ' = ' + tmpdist);
+
+        if (minDist > tmpdist) {
+            minDist = tmpdist;
+            minDistId = id;
+        } else if (minDist == tmpdist) {
+            minDistId = 0;
+        }
+    }
+
+    return minDistId;
+}
+
+function getSize(st: Storage, id: number): number {
+    let size = 0;
+    // console.log('Size: ' + st.storage.length);
+    for (let i = 0; i < st.storage.length; i++) {
+        for (let j = 0; j < st.storage[i].length; j++) {
+            // console.log('Checking x:' + i + ' y:' + j + ' value: ' + st.storage[i][j]);
+            if (st.storage[i][j] == id) {
+                if ((i == 0) || (j == 0) || (i == st.storage.length-1) || (j == st.storage[i].length-1)) return -1;
+                size++;
+            }
+        }
+    }
+
+    return size;
+}
+
+function execute(data) {
+    let maxc = getMax(data);
+    console.log('Max x:' + maxc[0] + ' y:' + maxc[1]);
+
+    // create storage
+    let storage = new Storage(maxc);
+
+    for (let i = 0; i < maxc[0]+1; i++) {
+        for (let j = 0; j < maxc[1]+1; j++) {
+            let id = getIdMinimalDistance(data, [i,j]);
+            storage.storage[i][j] = id;
+            // console.log('Point x:' + i + ' y:' + j + ' min dist to: '+ id + 'set to:'+ storage.storage[i][j]);
+        }
+    }
+
+    // storage.display();
+
+    let id = 0;
+    let maxSize = 0;
+    for (let line of data) {
+        id++;
+        let size = getSize(storage, id);
+        console.log('ID: ' + id + ' ' + getCoo(line) + ' size of: ' + size);
+        if (size > maxSize) maxSize = size;
+    }
+    console.log('Max: ' + maxSize);
+
+    let size = 0;
+    for (let i = 0; i < storage.storage.length; i++) {
+        for (let j = 0; j < storage.storage.length; j++) {
+            let dist = 0;
+            for (let line of data) {
+                dist += getDistance([i,j], getCoo(line));
+            }
+            if (dist < 10000) size++;
+        }
+    }
+    console.log('Size: ' + size);
+}
+
+execute(data);
 
