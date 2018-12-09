189bb5be85e48f252f4142265d88dafedde0ce6c
mcerdeiro <mcerdeiro@gmail.com>
2018-12-09 11:58:59 +0100

    day 8

--- day3.ts
+++ day3.ts
@@ -1,200 +1,81 @@
 import { dataTest } from "./data";
 import { data } from "./data";
 
-
-class Storage {
-    storage: number[][];
-
-    constructor([x, y], init = 0) {
-        x++;y++;
-        console.log('new storage for x: ' + x + ' y:' + y);
-        this.storage = new Array(x);
-        for (let i = 0; i < x; i++) {
-            this.storage[i] = new Array(y);
-            for ( let j = 0; j < y; j++) {
-                this.storage[i][j] = init;
-            }
-        }
-    }
-}
-
-interface dependency {
-    step: string;
-    dependsFromStep: string[];
-}
-
-interface worker {
-    working: string;
-    time: number;
-}
-
-function getDependency(line: string): dependency {
-    let tmp = line.split(' ');
-    return { step: tmp[7], dependsFromStep: [tmp[1]] };
-}
-
-function getNeededTime(letter: string): number {
-    return letter.charCodeAt(0)-'A'.charCodeAt(0) + 61;
-    // return letter.charCodeAt(0)-'A'.charCodeAt(0) +1;
-}
-
-function getStepWithoutDep(storage: dependency[]): dependency[] {
-    let stepsWithoutDep: dependency[];
-    stepsWithoutDep = new Array();
-
-    // search
-    for (let element of storage) {
-        if (element.dependsFromStep.length == 0) stepsWithoutDep.push(element);
-    }
-
-    return stepsWithoutDep;
-}
-
-function getStep(storage: dependency[]): {l: string, s: dependency[]} {
-    let stepsWithoutDep: dependency[];
-    stepsWithoutDep = new Array();
-
-    // search
-    for (let element of storage) {
-        if (element.dependsFromStep.length == 0) stepsWithoutDep.push(element);
-    }
-
-    if (stepsWithoutDep.length == 0) return { l: '', s: storage };
-
-    stepsWithoutDep.sort((a,b) => (a.step > b.step) ? 1 : 0);
-    console.log(stepsWithoutDep[0].step);
-
-    // remove
-    storage = storage.filter((a) => a.step != stepsWithoutDep[0].step);
-
-    return { l: stepsWithoutDep[0].step, s: storage };
-}
-
-
-function done(storage: dependency[], done: string): dependency[] {
-       // remove
-       storage = storage.filter((a) => a.step != done);
-       for (let element of storage) {
-           element.dependsFromStep = element.dependsFromStep.filter(a => a != done);
-       }
-   
-       // display
-       /*for (let element of storage) {
-           console.log(element);
-       }*/
-   
-       return storage;
-}
-
-function getNextStep(storage: dependency[]): {l: string, s: dependency[]} {
-    let stepsWithoutDep: dependency[];
-    stepsWithoutDep = new Array();
-
-    // search
-    for (let element of storage) {
-        if (element.dependsFromStep.length == 0) stepsWithoutDep.push(element);
-    }
-
-    if (stepsWithoutDep.length == 0) return { l: '', s: storage };
-
-    stepsWithoutDep.sort((a,b) => (a.step > b.step) ? 1 : 0);
-    console.log(stepsWithoutDep[0].step);
-
-    // remove
-    storage = storage.filter((a) => a.step != stepsWithoutDep[0].step);
-    for (let element of storage) {
-        element.dependsFromStep = element.dependsFromStep.filter(a => a != stepsWithoutDep[0].step);
-    }
-
-    // display
-    /*console.log('exit:');
-    for (let element of storage) {
-        console.log(element);
-    }*/
-
-    return { l: stepsWithoutDep[0].step, s: storage };
-}
-
-function execute(data) {
-    let storage: dependency[];
-    storage = new Array();
-
-    for (let line of data) {
-        let dep = getDependency(line);
-        let found = storage.find(val => val.step == dep.step);
-        if (found) {
-            if (found.dependsFromStep.find(step => step == dep.step[0])) {
-
-            } else {
-                found.dependsFromStep.push(dep.dependsFromStep[0]);
-            }
-        } else {
-            storage.push(dep);
-        }
-
-        found = storage.find(val => val.step == dep.dependsFromStep[0]);
-        if (!found) storage.push({ step: dep.dependsFromStep[0], dependsFromStep: new Array() });
-    }
-
-    let time = 0;
+class TreeElement {
+    countChilds: number;
+    dataLength: number;
+    childs: TreeElement[];
+    parent: TreeElement;
+    data: number[];
     
-    let workers: worker[] = [ { working: '', time: 0 }, { working: '', time: 0 }, { working: '', time: 0 }, { working: '', time: 0 }, { working: '', time: 0 } ];
-    while(1) {
+    length: number;
+    sum: number;
+    sumChilds: number;
 
-        {
-            let todos = getStepWithoutDep(storage);
-            let notWorking = workers.filter(a => a.working == '');
-            do {
-                console.log('check if we can improve');
-                console.log(workers);
-                console.log(todos);
-                console.log(notWorking);
-                /* check if we can improve */
-                if ( (notWorking.length > 0) || ( todos.length > 0)) {
-                    console.log('improvving');
-                    let ns = getStep(storage);
-                    storage = ns.s;
-                    workers[workers.findIndex(a => (a.working == ''))].time = getNeededTime(ns.l);
-                    workers[workers.findIndex(a => (a.working == ''))].working = ns.l;
-                }
+    pro2: number;
 
-                todos = getStepWithoutDep(storage);
-                notWorking = workers.filter(a => a.working == '');
-            }
-            while ( (todos.length != 0) && (notWorking.length != 0) );
+    constructor(code: number[]) {
+        this.countChilds = code[0];
+        this.dataLength = code[1];
+
+        this.data = new Array<number>();
+        this.childs = new Array<TreeElement>();
+
+        this.length = 2;
+        this.sum = 0;
+        this.sumChilds = 0;
+        this.pro2 = 0;
+
+        console.log(code);
+        console.log('Constructor TotalChilds:' + this.countChilds + ' current childs count: ' + this.childs.length);
+        while (this.countChilds != this.childs.length) {
+            let subArray = code.slice(this.length, code.length);
+            let child = new TreeElement(subArray);
+            this.childs.push(child);
+
+            this.length += child.length;
+            this.sumChilds += child.sum + child.sumChilds;
         }
 
-        {
-            let todos = getStepWithoutDep(storage);
-            let notWorking = workers.filter(a => a.working == '');
-            do {
-                console.log('Time: ' + time);
-                console.log('Workers');
-                console.log(workers);
-                console.log('TODOS');
-                console.log(todos);
-                /* move time */
-                time++;
-                for (let i = 0; i < workers.length; i++) {
-                    if (workers[i].working != '') {
-                        workers[i].time--;
-                        if (workers[i].time == 0) {
-                            storage = done(storage, workers[i].working);
-                            workers[i].time = -1;
-                            workers[i].working = '';
-                        }
-                    }
+        while (this.dataLength != this.data.length) {
+            this.sum += code[this.length]
+            this.data.push(code[this.length]);
+            this.length++;
+        }
+        //console.log('Data: ');
+        //console.log(this.data);
+
+        if (this.childs.length == 0) {
+            this.pro2 = this.data.reduce((a,b) => a+b);
+        } else {
+            for (let d of this.data) {
+                if (this.childs[d-1] != undefined) {
+                    this.pro2 += this.childs[d-1].pro2;
                 }
-
-                todos = getStepWithoutDep(storage);
-                notWorking = workers.filter(a => a.working == '');
-
-                if ((todos.length == 0) && (notWorking.length == workers.length)) return time;
             }
-            while ( (todos.length == 0) || (notWorking.length == 0) );
         }
 
     }
+}
+
+function convert(data: string): number[] {
+    let ret: number[];
+    ret = new Array<number>();
+    for (let code of data.split(' ')) {
+        ret.push(Number(code));
+    }
+    
+    return ret;
+}
+
+function execute(data: string): number {
+    let code = convert(data);
+    let root = new TreeElement(code);
+
+    console.log('Summ: ' + (root.sum + root.sumChilds));
+    console.log('Pr2: ' + (root.pro2));
+
+    return 0;
 }
 
 let str = execute(data);
