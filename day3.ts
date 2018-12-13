7cd0a70c1f52af36ca3e0cd74286cb550a31fd0b
mcerdeiro <mcerdeiro@gmail.com>
2018-12-13 20:39:20 +0100

    day 12

--- day3.ts
+++ day3.ts
@@ -1,79 +1,134 @@
 import { dataTest } from "./data";
 import { data } from "./data";
 
-interface cord {
-    x: number;
-    y: number;
-}
-
-class Point {
-    pos: cord;
-    vel: cord;
-
-    constructor(p: cord, v: cord) {
-        this.pos = p;
-        this.vel = v;
+function display(p: boolean[]) {
+    let str = '';
+    for (let i = 0; i < p.length; i++) {
+        str += p[i] ? '#' : '.';
     }
+    console.log(str);
 }
 
-let storage: Point[];
+function process(pots: boolean[], lives: boolean[][], dies: boolean[][]): boolean[] {
+    let ret = new Array<boolean>(pots.length)
+    for (let i = 0; i < ret.length; i++) {
+        ret[i] = false;
+    }
 
-function GetInfo(line: string): {p: cord, v:cord} {
-    // position=< 9,  1> velocity=< 0,  2>
-    let pos = line.split('> velocity=')[0].split('osition=<')[1].split(',');
-    let vel = line.split('> velocity=<')[1].split('>')[0].split(',');
-    //console.log('Line: ' + line);
+    for (let i = 2; i < pots.length-2; i++) {
+        let tmp =  new Array<boolean>();
+        tmp.push(pots[i-2]);
+        tmp.push(pots[i-1]);
+        tmp.push(pots[i-0]);
+        tmp.push(pots[i+1]);
+        tmp.push(pots[i+2]);
 
-    let po: cord = { x: Number(pos[0]), y: Number(pos[1]) };
-    let ve: cord = { x: Number(vel[0]), y: Number(vel[1]) };
+        // console.log('Search for in pos:' + i);
+        // console.log(tmp);
+        let l = lives.filter(a => {
+            for (let i = 0; i < a.length; i++) {
+                if (a[i] != tmp[i])
+                    return false;
+            }
+            // console.log('Lives: ');
+            // console.log(a);
+            return true;
+            }).length;
+        let d = dies.filter(a => {
+            for (let i = 0; i < a.length; i++) {
+                if (a[i] != tmp[i])
+                    return false;
+            }
+            // console.log('Dies: ');
+            // console.log(a);
+            return true;
+            }).length;
+        if (l > 0) {
+            ret[i] = true;
+        }
+    }
 
-    //console.log(po)
-    //console.log(ve)
+    for (let i = 0; i < ret.length; i++) {
+        pots[i] = ret[i];
+    }
 
-    return {p: po, v: ve};
+    return ret;
 }
 
-function display(c: cord, storage: Point[]) {
+function run(data: string[]) {
+    const diff = 4000;
+    let pots: boolean[];
+    let lives: boolean[][];
+    let dies: boolean[][];
 
-    for(let y = c.y; y < c.y+30; y++) {
-        let str = '';
-        let points = storage.filter(a => a.pos.y == y);
-        for(let x= c.x; x < c.x+120; x++) {
-            let found = points.find(a => a.pos.x == x);
-            if (found != undefined) {
-                // console.log('X: ' + x + ' Y: ' + y);
-                // console.log(found);
-                str += '#';
-            } else {
-                str += '.';
+    lives = Array<boolean[]>();
+    dies = Array<boolean[]>();
+
+    pots = new Array<boolean>();
+    for (let i = 0; i < diff; i++) {
+        pots.push(false);
+    }
+
+    let initStates = data[0].split('nitial state: ')[1].split('');
+    for (let initState of initStates) {
+        if(initState == '#') {
+            pots.push(true);
+        } else {
+            pots.push(false);
+        }
+    }
+
+    for (let i = 0; i < diff; i++) {
+        pots.push(false);
+    }
+
+    display(pots);
+
+    data.shift();
+
+    for (let d of data) {
+        let tmp = d.split(' => ');
+        let lord: boolean[];
+        lord = new Array<boolean>();
+        for (let val of tmp[0].split('')) {
+            if (val == '#')
+                lord.push(true);
+            else
+                lord.push(false);
+        }
+        if (tmp[1] == '#')
+            lives.push(lord);
+        else
+            dies.push(lord);
+    }
+
+    console.log(lives);
+    console.log(dies);
+
+    display(pots);
+    let tmp_old = 0;
+    for(let i = 0; i < 1500; i++) {
+        process(pots, lives, dies);
+        display(pots);
+        let tmp = 0;
+        for(let i = 0; i < pots.length; i++) {
+            if (pots[i] == true) {
+                tmp += i-diff;
             }
         }
-        console.log(str);
+        let dif = tmp-tmp_old;
+//        console.log('I:' + (i+1) + ' Summ: ' + tmp + ' diff: ' + (dif));
+        tmp_old = tmp;
+
+        
     }
+    display(pots);
+
+
+
 }
 
-function run(d: string[]) {
-    storage = new Array<Point>(d.length);
-    for (let i = 0; i < d.length; i++) {
-        let data = GetInfo(d[i]);
-        storage[i] = new Point(data.p, data.v);
-    }
-
-    let time = 0;
-    while(1) {
-        let minX = storage.reduce((a,b) => (a.pos.x > b.pos.x) ? b : a );
-        let minY = storage.reduce((a,b) => (a.pos.y > b.pos.y) ? b : a );
-        console.log('Time: ' + time);
-        display({x: minX.pos.x, y: minY.pos.y}, storage);
-
-        storage.map(a => {
-            a.pos.x = a.pos.x + a.vel.x;
-            a.pos.y = a.pos.y + a.vel.y;
-        });
-        time ++;
-
-    }
-
-}
 
 run(data);
+// let gen = 50000000000;
+// console.log(8990+58*(gen-123));
